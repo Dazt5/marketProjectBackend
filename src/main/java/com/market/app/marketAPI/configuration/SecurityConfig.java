1a3a8895a3128aaf0ead74dc0b4745c52294a1c7
David Alvarez <david.alvarez@valid.com>
2021-10-03 21:33:24 -0500

    ft: added cors to Spring Security in SecurityConfig

--- src/main/java/com/market/app/marketAPI/configuration/SecurityConfig.java
+++ src/main/java/com/market/app/marketAPI/configuration/SecurityConfig.java
@@ -13,7 +13,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable()
+        http.cors().and().csrf().disable()
                 .addFilterAfter(new JWTAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class)
                 .authorizeRequests()
                 .antMatchers(HttpMethod.POST, "/auth/**").permitAll()
